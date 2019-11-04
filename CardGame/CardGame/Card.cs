48c3a38b4973c925b10d1537caec1af4505c6e03
JonasManley <jonasmanley@hotmail.com>
2019-11-04 18:36:22 +0100

    Card class created

---
+++ CardGame/CardGame/Card.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Card
+    {
+        private int _cardValue;
+        private string _cardColor;
+
+        public Card(int cardvalue, string color)
+        {
+            _cardValue = cardvalue;
+            _cardColor = color;
+        }
+
+        public int cardValue
+        {
+            get { return _cardValue; }
+            set { _cardValue = value; }
+        }
+
+        public string cardColor
+        {
+            get { return _cardColor; }
+            set { _cardColor = value; }
+        }
+
+    }
