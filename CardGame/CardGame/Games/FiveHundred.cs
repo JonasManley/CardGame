b79998009c7bc8fe11d9907796bbd071d468526d
JonasManley <54889189+JonasManley@users.noreply.github.com>
2019-11-06 16:38:16 +0100

    Five Hundred game class
    Started working on the setup of the game and a little bit of the logic (but found out that cardValue is a string, which has to be int - and I don't want to convert it within the card class, that has to be done in the reader class where we read the deck)

---
+++ CardGame/CardGame/Games/FiveHundred.cs
@@ -0,0 +1,86 @@
+﻿using CardGame.Model;
+using Model.CardGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.Games
+{
+    class FiveHundred
+    {
+        private Player _a;
+        private Player _b;
+        private List<Card> _aCards;
+        private List<Card> _bCards;
+        private CardDeck _LoadedCardDeck;
+        private List<Card> _cardList;
+        private List<Card> _cardListShuffled;
+
+        public FiveHundred(Player A, Player B)
+        {
+            _LoadedCardDeck = new CardDeck();
+            _cardList = _LoadedCardDeck.cardList;
+            _cardListShuffled = _LoadedCardDeck.shuffledCardList;
+            _a = A;
+            _b = B;
+        }
+
+        private void GameLogic()
+        {
+            while(_aCards.Count > 0 || _bCards.Count > 0)
+            {
+                if (_aCards.IndexOf[0] > _bCards.IndexOf[0])
+                {
+
+                }
+                {
+
+                }
+               
+            }
+
+        }
+
+        public void play()
+        {
+            Setup();
+
+        }
+
+        private void Setup()
+        {
+            Console.WriteLine("loading card set...");
+            Console.WriteLine(_LoadedCardDeck.cardListLength + " Loaded cards");
+            Console.WriteLine("Shuffling cards...");
+            _LoadedCardDeck.Shuffel(new Random(), _LoadedCardDeck.cardList);
+            Console.WriteLine("Card deck have been shuffled");
+            Console.WriteLine($"Gives cards to {_a.name} and {_b.name}");
+            giveCards();
+            Console.WriteLine($"A's amount of cards dealt: {_a.cardAmount} ");
+            Console.WriteLine($"B's amount of cards dealt: {_b.cardAmount} ");
+            Console.WriteLine("------------------Ready to Play------------------");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Gaming is Started...");
+        }
+        /// <summary>
+        /// Logic for dealing cards
+        /// </summary>
+        private void giveCards()
+        {
+            foreach (var card in _cardListShuffled)
+            {
+                if (_a.cardAmount == _b.cardAmount)
+                {
+                    _a.Add(card);
+                }
+                else
+                {
+                    _b.Add(card);
+                }
+            }
+        }
+    }
+}
--- CardGame/CardGame/Program.cs
+++ CardGame/CardGame/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CardGame.Games;
+using CardGame.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,24 +13,32 @@ namespace CardGame
     {
         static void Main(string[] args)
         {
-            CardDeck cardDeck = new CardDeck();
-            Console.WriteLine("Reading from textfile...");
-            Thread.Sleep(2000);
-            var deck = cardDeck.cardList;
-            foreach (var card in deck)
-            {
-                Console.WriteLine(card);
-            }
-            Thread.Sleep(2000);
-            Console.WriteLine("--------------------");
-            Console.Clear(); Console.WriteLine("Shuffling deck..."); Console.WriteLine();
-            Thread.Sleep(2000);
+            //CardDeck cardDeck = new CardDeck();
+            //Console.WriteLine("Reading from textfile...");
+            //Thread.Sleep(2000);
+            //var deck = cardDeck.cardList;
+            //foreach (var card in deck)
+            //{
+            //    Console.WriteLine(card);
+            //}
+            //Thread.Sleep(2000);
+            //Console.WriteLine("--------------------");
+            //Console.Clear(); Console.WriteLine("Shuffling deck..."); Console.WriteLine();
+            //Thread.Sleep(2000);
 
-            cardDeck.Shuffel(new Random(), cardDeck.cardList);
-            foreach (var item in cardDeck.shuffledCardList)
-            {
-                Console.WriteLine(item);
-            }
+            //cardDeck.Shuffel(new Random(), cardDeck.cardList);
+            //foreach (var item in cardDeck.shuffledCardList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            Player A = new Player("Jonas");
+            Player B = new Player("Valde");
+
+            FiveHundred fiveHundred = new FiveHundred(A, B);
+
+            fiveHundred.play();
 
 
             Console.ReadLine();
