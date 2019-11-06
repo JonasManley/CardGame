9476bd761645febbe46638095c9d43e886826fd3
JonasManley <54889189+JonasManley@users.noreply.github.com>
2019-11-06 15:45:41 +0100

    Player class added

---
+++ CardGame/CardGame/Model/Player.cs
@@ -0,0 +1,55 @@
+﻿using Model.CardGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.Model
+{
+    class Player
+    {
+        private string _name;
+        private List<Card> _assignedCards;
+
+        public Player(string name)
+        {
+            _assignedCards = new List<Card>();
+        }
+
+        /// <summary>
+        /// Propperties
+        /// </summary>
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public List<Card> assignedCards
+        {
+            get { return _assignedCards; }
+        }
+        public int cardAmount
+        {
+            get { return _assignedCards.Count; }
+        }
+
+        /// <summary>
+        /// Methods 
+        /// </summary>
+        public void RemoveCard(Card card)
+        {
+            _assignedCards.Remove(card);
+        }
+
+        public void Add(Card card)
+        {
+            _assignedCards.Add(card);
+        }
+
+        public Card NextCard(int i)
+        {
+            return _assignedCards[i];
+        }
+    }
+}
